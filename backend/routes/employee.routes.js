07210373e345563dc3e4f9c3dadbf1c40143f71c
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-20 17:51:02 +0300

    read employee

--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -3,6 +3,7 @@ const Employee = require("../models/employee.model");
 
 const router = express.Router();
 
+// Add / Create New Employee
 router.route('/addEmployee').post(async function(req, res) {
   const { id, name, dateOfBirth, gender, salary } = req.body;
 
@@ -18,5 +19,14 @@ router.route('/addEmployee').post(async function(req, res) {
   } catch(err) {
     res.status(400).json({ message: err.message });
   }
-  
+
+});
+
+router.route('/getEmployees').get(async function(req, res) {
+  try {
+    const employees = await Employee.find();
+    res.status(201).send(JSON.stringify(employees));
+  } catch(err) {
+    res.status(400).json({ message: err.message });
+  }
 })
