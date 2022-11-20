493aac615dcacc022eb81240250d4945fa0d0885
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-20 18:31:29 +0300

    update employee

--- backend/routes/employee.routes.js
+++ backend/routes/employee.routes.js
@@ -29,4 +29,24 @@ router.route('/getEmployees').get(async function(req, res) {
   } catch(err) {
     res.status(400).json({ message: err.message });
   }
+});
+
+router.route('/updateEmployee/:id').post(async function (req, response) {
+  let id =req.params.id;
+  let newvalues = {
+    $set: {
+      name: req.body.name,
+      dateOfBirth: req.body.dateOfBirth,
+      gender: req.body.gender,
+      salary: req.body.salary,
+    }
+  }
+
+  try {
+    await (await Employee.updateOne({id: id}, newvalues)).then(function(err, res){
+      response.status(200).json(res)
+    })
+  } catch(err) {
+    response.status(400).json({ message: err.message });
+  }
 })
