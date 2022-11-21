d4bfff0f503d103680a2ff127198682cf273ddc8
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-21 13:56:45 +0300

    finished navbar

---
+++ frontend/src/components/Navbar/Nabvar.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  display: flex;
+  font-size: 28px;
+  padding: 20px;
+  justify-content: center;
+  color: white;
+  background-color: hsl(222,47%,11%);
+`
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav } from './Nabvar.styles';
+
+const Navbar:React.FC = (props) => {
+  return (
+    <Nav>
+      Addis Software PLC
+    </Nav>
+  );
+}
+
+export default Navbar;
