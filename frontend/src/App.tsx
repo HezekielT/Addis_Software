575541aa82b6024d286d1045544eed807e3fcf20
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-23 15:24:58 +0300

    app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import Employee from './components/Employee/Employee';
 import Navbar from './components/Navbar/Navbar';
-import { Provider } from 'react-redux';
 
 const App:React.FC = () => {
 
   return (
-    // <Provider store={store}>
     <>
       <Navbar />
       <Employee />
     </>
-    // </Provider>
   )
 }
 
