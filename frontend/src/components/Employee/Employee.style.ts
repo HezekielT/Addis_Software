5e6d94b693420a07a911e66ae973388e581c588d
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-23 15:25:12 +0300

    styling

--- frontend/src/components/Employee/Employee.style.ts
+++ frontend/src/components/Employee/Employee.style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Plus } from '@styled-icons/bootstrap'
 
 export const Container = styled.div`
   max-width: 976px;
@@ -9,6 +10,7 @@ export const Container = styled.div`
 export const Table = styled.table`
   border-collapse: collapse;
   width: 100%;
+  margin-bottom: 10px;
 `
 
 export const TableHeader = styled.th`
@@ -19,10 +21,27 @@ export const TableHeader = styled.th`
 export const TableData = styled.td`
   text-align: left;
   padding: 8px;
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 export const TableRow = styled.tr`
   &:nth-child(even) {
     background-color: hsl(215, 20%, 65%);
   }
+`
+export const Button = styled.button`
+  display: flex;
+  margin: auto;
+  padding: 10px;
+  width: auto;
+  color: white;
+  background-color: hsl(222,47%,11%);
+  &:hover {
+    cursor: pointer;
+  }
+`
+export const PlusIcon = styled(Plus)`
+  padding: 2px;
 `
