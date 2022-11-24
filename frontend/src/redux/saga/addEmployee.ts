9dbfa90fd0b8f844e1abbb25fadb7e05feeabb7f
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-25 01:01:22 +0300

    add saga

---
+++ frontend/src/redux/saga/addEmployee.ts
@@ -0,0 +1,30 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { Add } from '../actions/employeeAction';
+
+const apiUrl = `http://localhost:5000/addEmployee`
+
+const addApi = () => {
+  return fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    // body: Employee
+  }).then(response => response.json())
+  .catch((error) => {throw error})
+}
+
+function* fetchEmployees(action: Add) {
+  try{
+    const employees:string = yield call(addApi);
+    yield put({type: 'GET_EMPLOYEES', Employees: employees})
+  } catch(e: any) {
+    yield put({ type: 'GET_EMPLOYEES_FAILED', message: e.message})
+  }
+}
+
+function* employeeSaga() {
+  yield takeEvery('GET_EMPLOYEES_REQUESTED', fetchEmployees)
+}
+
+export default employeeSaga;
