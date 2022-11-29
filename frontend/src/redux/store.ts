c2d8f59db36f2208e55658a895590c0878bbc25a
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-30 01:23:32 +0300

    finished store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,6 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers';
+import rootSaga from './saga';
 
-const store = configureStore({ reducer: rootReducer });
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware]
+});
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
