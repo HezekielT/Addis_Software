6476d556aadaaa94931785d131fb2eadf6292126
hizkielT <hezekiel.feyissa@gmail.com>
2022-11-30 01:21:20 +0300

    added provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
+import store from './redux/store';
 // import 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  // </Provider>
+  </Provider>
 )
