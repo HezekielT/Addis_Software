cb31f5ca413109d44a8721889a1eb0e8d836c770
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-01 02:12:28 +0300

    our root saga

---
+++ frontend/src/redux/saga/index.ts
@@ -0,0 +1,10 @@
+import { all } from 'redux-saga/effects'
+import { watchFetchEmployee } from './GetEmployeeSaga'
+import { watchAddEmployee } from './AddEmployeeSaga'
+
+export default function* rootSaga() {
+  yield all([
+    watchFetchEmployee(),
+    watchAddEmployee()
+  ])
+}
