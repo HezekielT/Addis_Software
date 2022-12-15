f8a1dc58363701be8c31cb1790632d58705118b6
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-15 12:52:12 +0300

    types

--- frontend/src/redux/actions/types.ts
+++ frontend/src/redux/actions/types.ts
@@ -1,6 +1,6 @@
 import { EmployeeFields } from "../../components/Employee/Employee"
 
-export type GET_EMPLOYEES ={type: "GET_EMPLOYEES", payload: EmployeeFields[]}
+export type GET_EMPLOYEES ={type: "GET_EMPLOYEES", payload?: EmployeeFields[]}
 export type UPDATE_EMPLOYEES ={type: "UPDATE_EMPLOYEE", payload: EmployeeFields}
 export type ADD_EMPLOYEES ={type: "ADD_EMPLOYEE", payload: EmployeeFields}
 export type DELETE_EMPLOYEE = { type: "DELETE_EMPLOYEE", payload: string}
